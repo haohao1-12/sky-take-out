cae06eb85ff5298e36f973f603eaf46a5708ac78
Shenghao Li <LiShenghao2020@outlook.com>
2024-12-17 12:15:57 +0800

    新增菜品业务代码开发
    保护隐私

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -19,7 +19,7 @@ public interface CategoryMapper {
      * 插入分类数据
      * @param category
      */
-    @Insert("insert into sky_take_out.category (type, name, sort, status, create_time, update_time, " +
+    @Insert("insert into category (type, name, sort, status, create_time, update_time, " +
             "create_user, update_user) values (#{type}, #{name}, #{sort}, #{status}, " +
             "#{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     @AutoFill(value = OperationType.INSERT)
