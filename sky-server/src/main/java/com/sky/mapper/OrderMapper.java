829cf144b2229b51bb7146a832411aeade223720
Shenghao Li <LiShenghao2020@outlook.com>
2025-01-22 21:42:51 +0800

    订单支付业务功能开发（模拟支付过程）

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,10 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
 
 @Mapper
 public interface OrderMapper {
@@ -10,4 +14,28 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+
+    /**
+     * 根据订单号和用户id查询订单
+     * @param outTradeNo
+     * @return
+     */
+    @Select("select * from orders where number = #{outTradeNo} and user_id = #{userId}")
+    Orders getByNumberAndUserId(String outTradeNo, Long userId);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+    /**
+     *
+     * @param orderStatus
+     * @param orderPaidStatus
+     * @param checkoutTime
+     * @param id
+     */
+    @Update("update orders set status = #{orderStatus}, pay_status = #{orderPaidStatus}, checkout_time = #{checkoutTime} where id = #{id}")
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime checkoutTime, Long id);
 }
